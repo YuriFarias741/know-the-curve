e23418ec644e3b9cf8ac9b1a4edd59a5d80f7a33
Yuri Farias <yuricoelhodefarias@gmail.com>
2020-05-31 16:51:04 -0300

    Bufix urls

--- backend/app/brazil/urls.py
+++ backend/app/brazil/urls.py
@@ -7,6 +7,6 @@ app_name = 'brazil'
 
 urlpatterns = [
     path('', views.state_full_listing),
-    path('<str:state>', views.StateCasesTimeSeries.as_view(), name='time_series_cases_per_state'),
     path('demographic_density', views.DemographicDensityPerState.as_view(), name='demographic_density_vs_states'),
+    path('<str:state>', views.StateCasesTimeSeries.as_view(), name='time_series_cases_per_state'),
 ]
