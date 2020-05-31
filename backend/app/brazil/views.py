5beeb7abc8bc39b244e72c87fdc92502997f4d6f
Yuri Farias <yuricoelhodefarias@gmail.com>
2020-05-31 16:09:22 -0300

    Adiciona mortes e refatora endpoint home

--- backend/app/brazil/views.py
+++ backend/app/brazil/views.py
@@ -13,27 +13,30 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from brazil.consts import URL_LOCALIDADES, URL_POPULACAO, URL_COVID_STATUS
+from brazil.models import Occurrence
 from core.utils import export_plot
 
 @api_view(['GET',])
 def state_full_listing(request):
     today = timezone.localtime(timezone.now()).date()
-    week_list = [today - datetime.timedelta(days=x) for x in range(7)]
-    print(week_list)
-    week_occurrences_per_state = {}
-    for day in week_list:
-        response = requests.get(f'https://covid19-brazil-api.now.sh/api/report/v1/brazil/{day.strftime("%Y%m%d")}').json()
-        for data in response["data"]:
-            if not week_occurrences_per_state.get(data["uf"], None):
-                week_occurrences_per_state[data["uf"]] = {}
-            week_occurrences_per_state[data["uf"]][day.strftime("%d/%m/%Y")] = data["cases"]
+    month_list = [today - datetime.timedelta(days=x) for x in range(7)]
+    month_occurences_per_state = {}
+    for day in month_list:
+        daily_occurences = Occurrence.objects.filter(date_occured=day)
+        for occurence in daily_occurences:
+            if not month_occurences_per_state.get(occurence.state.state, None):
+                month_occurences_per_state[occurence.state.state] = {}
+            month_occurences_per_state[occurence.state.state][day.strftime("%d/%m/%Y")] = occurence.cases_count
+            print(occurence.cases_count)
     state_data = {}
-    for k, v in week_occurrences_per_state.items():
-        d = sorted(v.items(), key=lambda item: item[0])
-        datas = np.array([item[0] for item in d])
-        casos_variacao = np.array([item[1] for item in d])
-        casos = requests.get(f"https://covid19-brazil-api.now.sh/api/report/v1/brazil/uf/{k.lower()}/").json()["cases"]
-        state_data[k] = {"cases": casos, "slope": casos_variacao[-1] - casos_variacao[0]}
+    for k, v in month_occurences_per_state.items():
+        d = sorted(v.items(), key=lambda item: item[0]) # Order by date
+        case_array = [item[1] for item in d]
+        response = requests.get(f"https://covid19-brazil-api.now.sh/api/report/v1/brazil/uf/{k.lower()}/").json()
+        current_cases = response["cases"]
+        current_deaths = response["deaths"]
+        state_data[k] = {"cases": current_cases, "deaths": current_deaths, "slope": case_array[-1] - case_array[0]}
+        print(state_data)
     return Response(state_data, status=status.HTTP_200_OK)
 
 class DemographicDensityPerState(TemplateView):
